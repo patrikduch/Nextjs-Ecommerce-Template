5564058b21f6bb030d74b70dea34910510bccbab
patrikduch <duchpatrik@icloud.com>
2021-03-16 10:35:06 +0100

    Styled component: Footer content isolation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import HeaderTitle from '../components/common/typography/Header-Title'
 import BodyContainer from '../components/skeleton/body/Body-Container';
+import FooterContainer from '../components/skeleton/footer/Footer-Container';
 import { useTranslation } from '../i18n';
 
 const Home: React.FC = () => {
@@ -14,9 +15,9 @@ const Home: React.FC = () => {
           </HeaderTitle>
       </BodyContainer>
 
-      <footer>
+      <FooterContainer>
         <p> { t('author')}: Patrik Duch</p>
-      </footer>
+      </FooterContainer>
 
       <style jsx>{`
         .container {
@@ -26,58 +27,6 @@ const Home: React.FC = () => {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
         }
 
         @media (max-width: 600px) {
